b0dc7b46c6c940c87233de0730f2477c8ea6bf77
moonkyoungmi <cher1605@naver.com>
2025-02-28 17:53:11 +0900

    버튼 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
-import Test from './components/js/Test';
+import { Router, Route } from 'react-router-dom';
+import React, { useState, useRef, useReducer, useEffect } from 'react';
+import axios from 'axios';
+import Button from './components/Button';
 
 function App() {
-  return (
-    <div className="gray_background">
-      <h2>예시 컴포넌트</h2>
-      <p>src/components 폴더 밑에 컴포넌트.js 파일들 만들어서 사용하면 됩니다~</p>
-      <Test />
-      <p>~~</p>
-    </div>
-  );
+    return (
+        <div className="gray_background">
+            <h2>예시 컴포넌트</h2>
+            <p>src/components 폴더 밑에 컴포넌트.js 파일들 만들어서 사용하면 됩니다~</p>
+            <Button
+                text={'확인'}
+                onClick={() => {
+                    alert('!');
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,40 @@
+import { Button as MuiButton } from '@mui/material';
+
+/**
+ * 일반 버튼 컴포넌트
+ *
+ * @component
+ * @example
+ * return (
+ *   <Button text="확인" onClick={() => { alert("클릭"); }} />
+ * )
+ *
+ * @param {Object} props - 컴포넌트 속성
+ * @param {string} props.text - 버튼에 표시할 텍스트
+ * @param {() => void} props.onClick - 버튼 클릭 이벤트 핸들러
+ * @returns {JSX.Element} 스타일이 적용된 버튼 컴포넌트
+ */
+const Button = ({ text, onClick }) => {
+    return (
+        <div className="p-2">
+            <MuiButton
+                variant="contained"
+                disableElevation
+                onClick={onClick}
+                sx={{
+                    backgroundColor: '#5DB075',
+                    borderRadius: '30px',
+                    fontSize: '16px',
+                    fontWeight: 'bold',
+                    height: '50px',
+                    boxShadow: 'none',
+                    width: '100%',
+                }}
+            >
+                {text}
+            </MuiButton>
+        </div>
+    );
+};
+
+export default Button;
